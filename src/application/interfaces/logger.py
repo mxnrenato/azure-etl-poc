cd1629084db01947ab18db3bb4906aac4ee6ff4b
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-01 19:14:02 -0500

    feat: add Logger abstract class into application interface

---
+++ src/application/interfaces/logger.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Optional
+
+class Logger(ABC):
+    @abstractmethod
+    async def info(self, message: str, **kwargs: Dict[str, Any]) -> None:
+        pass
+
+    @abstractmethod
+    async def error(self, message: str, error: Optional[Exception] = None, **kwargs: Dict[str, Any]) -> None:
+        pass
+
+    @abstractmethod
+    async def warning(self, message: str, **kwargs: Dict[str, Any]) -> None:
+        pass
+
+    @abstractmethod
+    async def debug(self, message: str, **kwargs: Dict[str, Any]) -> None:
+        pass
