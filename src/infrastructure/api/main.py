f221a531ad770921c87187ea4dc4d6b8b2f5899d
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-02 12:53:30 -0500

    refact: improve the routes for main file

--- src/infrastructure/api/main.py
+++ src/infrastructure/api/main.py
@@ -2,31 +2,38 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.infrastructure.api.routes import employee_routes, backup_routes
 from src.infrastructure.api.middleware.error_handler import error_handler
-
 import azure.functions as func
 from azure.functions import AsgiMiddleware
 
-# Define la aplicación FastAPI
 app = FastAPI(
     title="ETL API",
     description="API for ETL operations",
-    version="1.0.0"
+    version="1.0.0",
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc"
 )
 
-# Configura el middleware
-app.middleware("http")(error_handler)  # Manejo de errores global
+app.middleware("http")(error_handler)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Cambia esto en producción para restringir orígenes
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Configura las rutas
-app.include_router(employee_routes.router, prefix="/api/v1/employees", tags=["employees"])
-app.include_router(backup_routes.router, prefix="/api/v1/system", tags=["system"])
+app.include_router(
+    employee_routes.router,
+    prefix="/api",
+    tags=["employees"]
+)
 
-# Conecta FastAPI con Azure Functions
+app.include_router(
+    backup_routes.router,
+    prefix="/api",
+    tags=["system"]
+)
+
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return AsgiMiddleware(app).handle(req, context)
