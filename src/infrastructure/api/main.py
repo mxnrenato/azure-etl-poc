98d626ade4ebdaa8d3f8db952d8f23cb0f48c9a4
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-03 02:01:44 -0500

    feat: include routes for backup

--- src/infrastructure/api/main.py
+++ src/infrastructure/api/main.py
@@ -5,6 +5,7 @@ from src.infrastructure.api.routes.ingest_routes import router as ingest_router
 from src.infrastructure.api.middleware.error_handler import error_handler
 import azure.functions as func
 from azure.functions import AsgiMiddleware
+from src.infrastructure.di.container import Container
 
 app = FastAPI(
     title="ETL API",
@@ -24,9 +25,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+container = Container()
+container.wire(modules=[backup_routes])
+
 app.include_router(employee_routes.router, prefix="/api", tags=["employees"])
 app.include_router(ingest_router, prefix="/api", tags=["Ingest"])
-app.include_router(backup_routes.router, prefix="/api", tags=["system"])
+app.include_router(backup_routes.router, prefix="/api", tags=["backup"])
 
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
