3dced5ba2890903ae93ece4b56bf28c6e9e7013c
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-03 03:07:36 -0500

    refact: change name by department

--- src/domain/entities/departament.py
+++ src/domain/entities/departament.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 @dataclass
 class Department:
     id: int
-    name: str
+    department: str  # Cambiado de 'name' a 'department'
 
     def __post_init__(self):
-        if not self.name.strip():
+        if not self.department.strip():  # Validar el nuevo nombre 'department'
             raise ValueError("Department name cannot be empty")
         if self.id <= 0:
             raise ValueError("Department ID must be positive")
