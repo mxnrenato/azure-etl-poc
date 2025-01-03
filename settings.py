39fb276a676a4d0adb76d0657a30acdef4107d65
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-03 02:02:03 -0500

    feat: add name and container for backup

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 AZURE_BLOB_CONTAINER_ROW_DATA = os.getenv("AZURE_BLOB_CONTAINER_ROW_DATA")
+AZURE_BLOB_CONTAINER_BACKUPS = os.getenv("AZURE_BLOB_CONTAINER_BACKUPS")
 AZURE_SQL_CONNECTION_STRING = os.getenv("AZURE_SQL_CONNECTION_STRING")
 
 # Validate required variables
@@ -14,7 +15,8 @@ if not all(
     [
         AZURE_STORAGE_CONNECTION_STRING,
         AZURE_BLOB_CONTAINER_ROW_DATA,
-        AZURE_SQL_CONNECTION_STRING,
+        AZURE_BLOB_CONTAINER_BACKUPS,
+        AZURE_SQL_CONNECTION_STRING
     ]
 ):
     raise ValueError("One or more required environment variables are missing!")
