9d680cc16584046e7bec00128ddcf8d4470cb122
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-03 03:43:19 -0500

    chore: keep update .gitignore

--- HttpTrigger/__init__.py
+++
@@ -1,7 +0,0 @@
-import azure.functions as func
-from azure.functions import AsgiMiddleware
-from src.infrastructure.api.main import app
-
-
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return AsgiMiddleware(app).handle(req, context)
