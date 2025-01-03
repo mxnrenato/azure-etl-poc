131265b12bcbc388443b58ddb4550b08074403e4
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-03 03:08:47 -0500

    refact: change name by job

--- src/domain/entities/job.py
+++ src/domain/entities/job.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 @dataclass
 class Job:
     id: int
-    name: str
+    job: str  # Cambiado de 'name' a 'job'
 
     def __post_init__(self):
-        if not self.name.strip():
+        if not self.job.strip():  # Validar el nuevo nombre 'job'
             raise ValueError("Job name cannot be empty")
         if self.id <= 0:
             raise ValueError("Job ID must be positive")
