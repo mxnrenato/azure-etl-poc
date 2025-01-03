e7206084dd7d86a17d0f094d50193f3c0e0b279f
Renato Maximiliano Rivera Abad <maximilianoriveraabad@gmail.com>
2025-01-03 01:11:19 -0500

    feat: add string conections for blob storage and sql database

---
+++ settings.py
@@ -0,0 +1,14 @@
+# settings.py
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+AZURE_BLOB_CONTAINER_ROW_DATA = os.getenv("AZURE_BLOB_CONTAINER_ROW_DATA")
+AZURE_SQL_CONNECTION_STRING = os.getenv("AZURE_SQL_CONNECTION_STRING")
+
+# Validate required variables
+if not all([AZURE_STORAGE_CONNECTION_STRING, AZURE_BLOB_CONTAINER_ROW_DATA, AZURE_SQL_CONNECTION_STRING]):
+    raise ValueError("One or more required environment variables are missing!")
